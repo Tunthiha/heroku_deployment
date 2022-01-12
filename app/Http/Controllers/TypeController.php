af8023ff4d1cfd8dda6a046c29d3588f5777d7ba
tunthiha <tunthiha9999@gmail.com>
2022-01-13 01:41:35 +0630

    return changes

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -44,7 +44,7 @@ class TypeController extends Controller
             'type' => $request->type
         ]);
         $types = Type::all();
-        return response()->json(['$type'=>$types]);
+        return response()->json([$types]);
     }
 
     /**
